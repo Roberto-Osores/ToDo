c51eceb51fb0fa006cd237d1ea881af75ba97ec4
Roberto <osoresroberto@gmail.com>
2024-10-24 10:56:20 -0300

    commit inicial del proyecto to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskItemComponent } from "./components/task-item/task-item.component";
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskItemComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ToDo';
+
+  items = [
+    { 
+      titulo: 'Lecturas OK', 
+      descripcion: 'Descripcion basica', 
+      backgroundColor: '#FFEE93',
+       
+    },
+    { 
+      titulo: 'Alertas medias', 
+      descripcion: 'Descripcion basica', 
+      backgroundColor: '#FFC09F', 
+      
+    },
+    { 
+      titulo: 'Alertas rojas', 
+      descripcion: 'Descripcion basica', 
+      backgroundColor: '#FCF5C7', 
+       
+    },
+    { 
+      titulo: 'Sensores deshabilitados', 
+      descripcion: 'Descripcion basica', 
+      backgroundColor: '#FCF5C7', 
+      
+    },
+  ];
 }
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [MatMenuModule, MatIconModule, MatButtonModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+
+  @Input() titulo: string = '';
+  @Input() descripcion: string = '';
+  @Input() backgroundColor: string = '';
+}
